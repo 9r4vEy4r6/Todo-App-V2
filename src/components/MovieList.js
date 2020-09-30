9ac3bd413ff8757e2a60f09bde840196c0e32b17
PratikGarai <pratikgarai0208@gmail.com>
2020-10-01 02:49:49 +0530

    Movie list is rendering properly now

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
+import movies from './movies.json';
 
 const MovieList = () =>
 {
+	const arr = movies.movies.map(movie => <p>{movie.name}</p>);
 	return(
-
+		[arr]
 	);
 }
 
