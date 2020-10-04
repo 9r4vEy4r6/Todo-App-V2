bb2783c62e42c819bd5e481709854d091a726cad
PratikGarai <pratikgarai0208@gmail.com>
2020-10-04 16:29:18 +0530

    set up the root reducer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {createStore} from 'redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
+
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers(counterReducer, loggedReducer);
+
+export default rootReducer;
