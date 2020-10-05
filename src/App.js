197f0d485513bee914668aa7f52200ec4a964258
PratikGarai <pratikgarai0208@gmail.com>
2020-10-05 08:58:08 +0530

    Completing the learninf reduc part

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './css/main.css';
 import './css/nav.css';
 
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement, login} from  './actions/index';
 
 function App() {
 	const counter = useSelector( state => state.counter );
-	const isLogged = useSelector( state=> state.isLogged );
+	const isLogged = useSelector( state=> state.logged );
+	const dispatch = useDispatch();
+
   	return (
 	  	<div>
+			<button onClick={()=>dispatch(increment())} > + </button>
+			<button onClick={()=>dispatch(decrement())} > - </button>
+			<button onClick={()=>dispatch(login())} > LogIn </button>
+
 			<p>{counter}</p>
 			{ isLogged ? <h3> Value to be seen only after LogIn</h3>: ""}
 		</div>
--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,7 @@ export const decrement = () => {
 	}
 }
 
-export const logToggle = () => {
+export const login = () => {
 	return {
 		type : 'SIGN_IN'
 	}
