c46a4b1c437a1c7cad008c3ee60093f67f76f069
PratikGarai <pratikgarai0208@gmail.com>
2020-10-07 11:13:32 +0530

    Removed error components

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -3,8 +3,6 @@ import React, {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {movieAdd} from '../actions/movieAddDeleteAction';
 
-import Error from './Error';
-
 const AdderForm = (props) =>
 {
 	const dispatch = useDispatch();
@@ -12,52 +10,33 @@ const AdderForm = (props) =>
 
 	const [name, setName] = useState("");
 	const [price, setPrice] = useState("");
-	const [errors, setErrors] = useState([]);
 
-	const changeHandler = (e) =>
+	const changeHandlerName = (e) =>
 	{
-		setErrors([]);
-		switch(e.target.name)
-		{
-			case "movie":
-				setName(e.target.value);
-				return;
-			case "price":
-				setPrice(e.target.price);
-				return;
-			default :
-				console.log(e.target.name+" no handler found in form");
-		}
+		setName(e.target.value);
+	}
+
+	const changeHandlerPrice = (e) =>
+	{
+		setPrice(e.target.value);
 	}
 
 	const submitForm = (e) =>
 	{
-		if(name.length>0)
-			setErrors( [ ...errors, "Name of movie is empty"] );
-		if(price.length>0)
-			setErrors( [ ...errors, "Price of movie is empty"] );
-		if(errors.length>0)
-			return;
-		console.log("Name : "+ name + "Price : " + price + "Errors : " + errors) 
-
 		const newObj = {
 			id : movies.length+1,
 			name : name,
 			price : price
 		}
 		dispatch(movieAdd(newObj));
-
 		setName("");
 		setPrice("");
 	}
 
 	return (
 		<div className = "form-block">
-			<div className = {(errors.length===0)?"":"error-block"}> 
-				{ errors.map(message => <Error message={message} />) }
-			</div>
-				<input type="text" name="movie" value={name} onChange={changeHandler}/>
-				<input type="text" name="price" value={price} onChange={changeHandler}/>
+				<input type="text" name="movie" value={name} onChange={changeHandlerName}/>
+				<input type="text" name="price" value={price} onChange={changeHandlerPrice}/>
 				<button onClick={submitForm}>Add Movie</button>
 		</div>
 	);
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -8,7 +8,7 @@ const MovieList = () =>
 	const movieState = useSelector(state => state.movies);
 	return(
 		<div className="movie-list">
-			{(movieState.length===0)?<h2>No movies in here!</h2>:movieState.map(movie => <Movie movie={movie}/>)}
+			{(movieState.length===0)?<h2>No movies in here!</h2>:movieState.map(movie => <Movie key={movie.id} movie={movie}/>)}
 		</div>
 	);
 }
