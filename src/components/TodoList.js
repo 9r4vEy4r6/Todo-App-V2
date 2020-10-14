0ac5d136f34dbad1abe08c83cfadbd950323b1f7
PratikGarai <pratikgarai0208@gmail.com>
2020-10-14 14:02:31 +0530

    added jquery hover effect

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Todo from './Todo';
+import $ from "jquery";
 
 import {useSelector} from 'react-redux';
 
 const TodoList = () =>
 {
 	const todoState = useSelector(state => state.todos);
+	
+	useEffect(()=>{
+		$(".todo-content")
+			.mouseover((e)=>{
+				$(e.target).toggleClass("todo-content-expand");
+				console.log(e.target);
+			});
+	});
+
 	return(
 		<div className="todo-list">
 			{(todoState.length===0)?<h2>No tasks pending!</h2>:todoState.map(todo => <Todo key={todo.id} todo={todo}/>)}
